2d2a2901da788ec5ce821b81e68e998b956ed60d
٩(◕‿◕｡)۶ <mail@noono.sh>
2025-05-10 14:50:39 +0100

    refactor fetchCars and related utility functions for improved readability and consistency

--- utils/index.ts
+++ utils/index.ts
@@ -1,63 +1,63 @@
 import { CarProps, FilterProps } from "@/types";
 
 export async function fetchCars(filters: FilterProps) {
-	const API_KEY: string | undefined = process.env.NEXT_PUBLIC_RAPIDAPI_KEY;
+  const API_KEY: string | undefined = process.env.NEXT_PUBLIC_RAPIDAPI_KEY;
 
-	const { manufacturer, year, model, limit, fuel } = filters;
+  const { manufacturer, year, model, limit, fuel } = filters;
 
-	if (!API_KEY) throw new Error("No Rapid API key specified.");
+  if (!API_KEY) throw new Error("No Rapid API key specified.");
 
-	const headers = {
-		"X-RapidAPI-Key": API_KEY,
-		"X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-	};
+  const headers = {
+    "x-rapidapi-key": API_KEY,
+    "x-rapidapi-host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
 
-	const response = await fetch(
-		`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
-		{ headers: headers },
-	);
+  const response = await fetch(
+    `https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`,
+    { headers: headers }
+  );
 
-	const result = await response.json();
+  const result = await response.json();
 
-	return result;
+  return result;
 }
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
-	const basePricePerDay = 40; // Base rental price per day in dollars
-	const mileageFactor = 1.3; // Additional rate per mile driven
-	const ageFactor = 0.95; // Additional rate per year of vehicle age
+  const basePricePerDay = 40; // Base rental price per day in dollars
+  const mileageFactor = 1.3; // Additional rate per mile driven
+  const ageFactor = 0.95; // Additional rate per year of vehicle age
 
-	// Calculate additional rate based on mileage and age
-	const mileageRate = city_mpg * mileageFactor;
-	const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
 
-	// Calculate total rental rate per day
-	const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
 
-	return rentalRatePerDay.toFixed(0);
+  return rentalRatePerDay.toFixed(0);
 };
 
 export const generateCarImageUrl = (car: CarProps, angle?: string) => {
-	const url = new URL("https://cdn.imagin.studio/getimage");
+  const url = new URL("https://cdn.imagin.studio/getimage");
 
-	const { make, year, model } = car;
+  const { make, year, model } = car;
 
-	url.searchParams.append("customer", "hrjavascript-mastery");
-	url.searchParams.append("make", make);
-	url.searchParams.append("modelFamily", model.split(" ")[0]);
-	url.searchParams.append("zoomType", "fullscreen");
-	url.searchParams.append("modelYear", `${year}`);
-	url.searchParams.append("angle", `${angle}`);
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
 
-	return `${url}`;
+  return `${url}`;
 };
 
 export const updateSearchParams = (type: string, value: string) => {
-	const searchParams = new URLSearchParams(window.location.search);
+  const searchParams = new URLSearchParams(window.location.search);
 
-	searchParams.set(type, value);
+  searchParams.set(type, value);
 
-	const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+  const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
 
-	return newPathName;
+  return newPathName;
 };
